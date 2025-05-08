4cbbe6e7d2ea4d211502d8c7087dc4c72c5f6f62
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-05-08 14:46:01 -0400

    Implment word class

---
+++ lib/word.py
@@ -0,0 +1,24 @@
+class Word:
+
+    def __init__(self, text):
+        assert isinstance(text, str), 'Word must be a string'
+        self._text = text
+        self._pairs = None
+
+    @property
+    def text(self):
+        return self._text
+
+    def __repr__(self):
+        return f'Word({self._text})'
+
+    def __str__(self):
+        return self._text
+
+    def get_pairs(self):
+        if self._pairs is None:
+            self._pairs = []
+            text = ['.'] + list(self._text[:]) + ['.']
+            for first, second in zip(text[:], text[1:]):
+                self._pairs.append((first, second))
+        return self._pairs
