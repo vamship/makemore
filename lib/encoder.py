a01337487deeeb60c0276de81a17c913787eafcf
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-05-10 21:49:48 -0400

    Add support for one hot encoding

--- lib/encoder.py
+++ lib/encoder.py
@@ -1,9 +1,12 @@
+import torch
+
 class Encoder:
 
     def __init__(self, vocabulary):
         assert isinstance(vocabulary,
                           (str, list)), 'Invalid vocabulary (arg #1)'
         self._char_lookup = list(vocabulary)
+        self._embeddings = torch.eye(len(vocabulary))
         self._char_map = {}
         for index, char in enumerate(self._char_lookup):
             self._char_map[char] = index
@@ -18,3 +21,9 @@ class Encoder:
     def get_char(self, index):
         assert index >= 0, 'Invalid index (arg #1)'
         return self._char_lookup[index]
+
+    def get_embedding(self, index):
+        if isinstance(index, str):
+            index = self.get_index(index)
+        assert index >= 0, 'Invalid index (arg #1)'
+        return self._embeddings[index]
