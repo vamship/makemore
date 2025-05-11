e5d86608c55e89f0489d15d46f21e861b142fea2
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-05-10 21:48:55 -0400

    Add supoprt for gobal initialization of random seeds

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,7 +1,19 @@
 __all__ = ['Word', 'Encoder', 'WordList', 'SimpleBigram']
+__all__ = [
+    'Word',
+    'Encoder',
+    'WordList',
+    'SimpleBigram',
+    'generate_words',
+    'calculate_loss',
+    'show_stats',
+    'init_random',
+    'global_generator',
+]
 
 from .word import Word
 from .encoder import Encoder
 from .word_list import WordList
 from .simple_bigram import SimpleBigram
 from .utils import generate_words, calculate_loss, show_stats
+from .utils import generate_words, calculate_loss, show_stats, init_random, global_generator
--- lib/utils.py
+++ lib/utils.py
@@ -1,4 +1,13 @@
+import torch
+
+global global_generator
+global_generator = torch.Generator()
+
+
 def generate_words(model, encoder, count, generator=None):
+    if generator is None:
+        generator = global_generator
+
     words = []
     for _ in range(count):
         index = encoder.get_index('.')
@@ -11,14 +20,16 @@ def generate_words(model, encoder, count, generator=None):
         words.append(''.join(chars))
     return words
 
+
 def calculate_loss(model, words):
     log_sum = 0.0
     count = 0
     for word in words:
         for pair in word.get_pairs():
             log_sum += model.get_log_likelihood(pair)
-            count +=1
+            count += 1
     return log_sum / count
+
 
 def show_stats(model, words):
     for word in words:
@@ -27,3 +38,11 @@ def show_stats(model, words):
             prob = model.get_probability(pair)
             likelihood = model.get_log_likelihood(pair)
             print(f'{pair}: {count=:>8}, {prob=:.4f}, {likelihood=:.4f}')
+
+
+def init_random(seed=1337):
+    import random
+
+    global global_generator
+    global_generator = global_generator.manual_seed(seed)
+    random.seed(seed)
