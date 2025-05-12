528b181d5685408a745f03ca5aca0d62b12f04f0
Vamshi K Ponnapalli <vamshi.ponnapalli@gmail.com>
2025-05-12 15:11:52 -0400

    Add method to prepare data for training

--- lib/__init__.py
+++ lib/__init__.py
@@ -4,11 +4,9 @@ __all__ = [
     'WordList',
     'SimpleBigram',
     'NeuronBigram',
-    'generate_words',
-    'calculate_loss',
-    'show_stats',
     'init_random',
     'global_generator',
+    'prepare_data',
 ]
 
 from .word import Word
@@ -16,4 +14,4 @@ from .encoder import Encoder
 from .word_list import WordList
 from .simple_bigram import SimpleBigram
 from .neuron_bigram import NeuronBigram
-from .utils import init_random, global_generator
+from .utils import init_random, global_generator, prepare_data
--- lib/utils.py
+++ lib/utils.py
@@ -3,9 +3,21 @@ import torch
 global global_generator
 global_generator = torch.Generator()
 
+
 def init_random(seed=1337):
     import random
 
     global global_generator
     global_generator = global_generator.manual_seed(seed)
     random.seed(seed)
+
+
+def prepare_data(words, transform):
+    input_chars = []
+    label_chars = []
+    for word in words:
+        for pair in word.get_pairs():
+            input_chars.append(pair[0])
+            label_chars.append(pair[1])
+
+    return transform(input_chars), transform(label_chars)
